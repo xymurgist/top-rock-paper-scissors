06114039460be2e9f8374a0579d914e13ccffbf6
“xymurgist” <pps.jim@gmail.com>
2024-02-04 15:17:39 -0700

    Prompt for player's choice.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,25 @@
-
+// Create an array with the choices
 const choices = ["Rock", "Paper", "Scissors"];
 
+// Get a random choice for the computer from the array
 function getComputerChoice() {
     let index = Math.floor(Math.random() * choices.length);
     return choices[index];
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+    let playerChoice = prompt("Pick one: Rock, Paper, Scissors");
+    return playerChoice;
+}
+
+// console.log(getComputerChoice());
+
+
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection);
+    console.log(computerSelection);
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
