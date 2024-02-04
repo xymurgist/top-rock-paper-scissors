91b57e99d872d12013f28822692e3ea071461c4f
“xymurgist” <pps.jim@gmail.com>
2024-02-04 16:53:01 -0700

    Play 5 round game and display winner.

--- scripts.js
+++ scripts.js
@@ -18,35 +18,59 @@ function getPlayerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let answer = ""
+    let answer = "";
+    let computerScore = 0;
+    let playerScore = 0;
+    let gameArray = [];
+
     if ((playerSelection == "Rock" && computerSelection == "Rock")
         || (playerSelection == "Paper" && computerSelection == "Paper")
         || (playerSelection == "Scissors" && computerSelection == "Scissors")) {
-        answer = `It's a Tie! ${playerSelection} equals ${computerSelection}`
+        answer = `This round is a Tie! ${playerSelection} equals ${computerSelection}`
     } else if ((playerSelection == "Rock" && computerSelection == "Paper")
         || (playerSelection == "Paper" && computerSelection == "Scissors")
         || (playerSelection == "Scissors" && computerSelection == "Rock")) {
-        answer = `You Lose! ${playerSelection} is beaten by ${computerSelection}`
+        answer = `You Lose this round! ${playerSelection} is beaten by ${computerSelection}`
+        computerScore = 1
     } else if ((playerSelection == "Rock" && computerSelection == "Scissors")
         || (playerSelection == "Paper" && computerSelection == "Rock")
         || (playerSelection == "Scissors" && computerSelection == "Paper")) {
-        answer = `You WIN! ${playerSelection} beats ${computerSelection}`
+        answer = `You WIN this round! ${playerSelection} beats ${computerSelection}`
+        playerScore = 1
     }
+
+    gameArray = [answer, computerScore, playerScore];
     
-    return answer;
+    return gameArray;
     
 }
 
 function playGame() {
+    let computerScore = 0;
+    let playerScore = 0;
+
     for (let i = 0; i < 5; i++) {
         const playerSelection = getPlayerChoice();
         const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
+
+        let gameRound = playRound(playerSelection, computerSelection);
+        let roundAnswer = gameRound[0];
+        let computerRound = gameRound[1];
+        let playerRound = gameRound[2];
+
+        computerScore += computerRound;
+        playerScore += playerRound;
+
+        console.log(roundAnswer);
+    }
+
+    if (computerScore > playerScore) {
+        console.log(`You Lose the game! ${computerScore} to ${playerScore}`);
+    } else if (playerScore > computerScore) {
+        console.log(`You Win the game! ${playerScore} to ${computerScore}`);
+    } else {
+        console.log(`The game ends in a Tie! ${playerScore} to ${computerScore}`);
     }
 }
 
-// const playerSelection = getPlayerChoice();
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
-// console.log(playGame());
 playGame();
